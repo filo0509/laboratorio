0c238dd4aa0b78eb8bc45cba54306a492984e0ed
filo0509 <filippo.donati@liceodongnocchi.eu>
2022-05-30 13:13:56 +0200

    finale

--- sito.py
+++ sito.py
@@ -1,3 +1,5 @@
+# manca da capire come funziona l'attrito con le sfere
+
 import pandas as pd
 import plotly.express as px
 import streamlit as st
@@ -11,19 +13,27 @@ h = 0.35
 
 v1 = sqrt(2 * 9.81 * h)
 
+error_x = 0.001
+error_y = 0.001
+
 st.title("Laboratorio di fisica: urto con pendolo")
 
 # in the sidebar of streamlit app put a radio to decide the file to open
 # st.sidebar.radio("Select the file to open", ["data_1", "data_2", "data_3"])
 
-file = st.sidebar.radio("Select the file to open", [
+file = st.sidebar.radio("Selezione il file da aprire", [
                         "data_1.csv", "data_2.csv", "data_3.csv", "data_4.csv", "data_5.csv", "data_6.csv"])
 
 df = pd.read_csv("data/def/" + file)
 df = df.dropna()
 
-st.write("L'obbiettivo del nostro esperimento era di determinare la velocità con cui la massa urtata si muoveva dopo l'urto.")
+st.caption("L'obbiettivo dell'esperimento è verificare che la velocità della massa urtata rispetti la conservazione del momento.")
+st.subheader("Procedimento: ")
+st.write("Si è utilizzata una massa di 0.07804 kg collegata ad un filo lungo 35 cm per urtare una massa di 1.36916 kg. Lasciando oscillare la massa attaccata al pendolo essa urta con la massa maggiore" +
+         ", muovendola con una velocità v2")
 
+st.write("L'azione dell'attrito non ha grande rilevanza, come si può notare dal grafico."
+         + "La linea di tangenza infatti fitta i dati.")
 # plot time and position of df as x and y axis using plotly express on streamlit app
 position_graph = px.scatter(
     df, x="time", y="position", color="position", trendline="ols")
@@ -33,13 +43,15 @@ st.plotly_chart(position_graph)
 model_position = LinearRegression()
 model_position.fit(df[["time"]], df[["position"]])
 # write the accuracy of the model
-st.write("accuracy of the model:", model_position.score(
+st.write("Accuratezza del modello:", model_position.score(
     df[["time"]], df[["position"]]))
 
+st.write("Equazione della retta di regressione: ")
 # latex formula for the model on streamlit app
 st.latex("y = " + str(model_position.coef_[0]) +
          "x + " + str(model_position.intercept_))
 
+st.write("")
 # add a new column named velocity to df that is the variation of position divided by the variation of time
 df["velocity"] = round(df["position"].diff() / df["time"].diff(), 4)
 df = df.iloc[1:]
@@ -55,6 +67,7 @@ model_velocity.fit(df[["time"]], df[["velocity"]])
 st.write("accuracy of the model:", model_velocity.score(
     df[["time"]], df[["velocity"]]))
 
+st.write("Equazione della retta di regressione: ")
 # latex formula for the model on streamlit app
 st.latex("y = " + str(model_velocity.coef_[0]) +
          "x + " + str(model_velocity.intercept_))
@@ -66,6 +79,14 @@ st.write("average of velocity:", round(df["velocity"].mean(), 4))
 v2 = v1 * (m1 + m1) / (m1 + m2)
 
 st.write("La velocità che ci aspettavamo è: ", round(-v2, 4))
+
+# calculate the measure error of velocity with standard deviation
+st.write("La deviazione standard è: ", round(df["velocity"].std(), 4))
+
+# calculate the measure error of velocity with standard error
+st.write("La semidispersione massima è: ", round(df["velocity"].sem(), 4))
+
+# calculate the measure error of velocity with error_x and error_y for position and time
 
 # add a new column named acceleration to df that is the variation of velocity divided by the variation of time
 df["acceleration"] = round(df["velocity"].diff() / df["time"].diff(), 4)
@@ -79,3 +100,22 @@ st.plotly_chart(acceleration_graph)
 # find the average of acceleration column
 avg_acceleration = df["acceleration"].mean()
 st.write("Accelerazione media", avg_acceleration)
+
+st.write("Il dataset è: ")
+st.dataframe(df)
+
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+
+csv = convert_df(df)
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name=file,
+    mime='text/csv',
+)
