c0d0ad19600a7b26b923541bbbbc31b604b82408
filo0509 <filippo.donati@liceodongnocchi.eu>
2022-08-07 10:24:59 +0200

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -4,11 +4,6 @@ from math import *
 
 plt.style.use("seaborn")
 
-# incertezza di misura della velocità: 0.5cm, ovvero 0.005m
-# la misura del tempo è in secondi e ha un errore di misura di 0.0001s, perchè le misurazioni sono state prese a 9600 baud
-# la velocità "istantanea" è in m/s, quindi il suo errore relativo è uguale alla somma degli errori relativi di dividendo e divisore
-
-
 m1 = 0.14904
 m2 = 1.36916
 
@@ -22,17 +17,12 @@ df = pd.read_csv('data/data_5.csv')
 df['position'] = df['position'].values[::-1]
 
 df_20 = df.head(20)
+print(df_20)
 # calculate the mean of the velocity column of df_20
 avg_velocity = round(df_20["velocity"].mean(), 5)
 v2 = round(v1 * (m1 + m1) / (m1 + m2), 5)
-
-
-# find the difference between the average velocity and all the values of the column 'velocity'
-df_20['velocity_error'] = abs(df_20['velocity'] - avg_velocity)
-# find the mean of the column 'velocity_error'
-avg_velocity_error = round(df_20['velocity_error'].mean(), 5)
-print(avg_velocity_error)
-
+print(avg_velocity)
+print(v2)
 
 plt.plot(df_20["time"], df_20["position"])
 plt.show()
